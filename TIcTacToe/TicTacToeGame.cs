d1ff6d461d70d3ffd5162a562a6340bef6d7313f
thomasnass <thomas.nass@winlas.se>
2024-09-26 07:47:39 +0200

    board initialization
    a 3x3 board can now be initialized

---
+++ TIcTacToe/TicTacToeGame.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class TicTacToeGame
+    {
+        private char[,] _board;
+
+        public TicTacToeGame()
+        {
+            _board = new char[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    _board[i, j] = ' ';
+                }
+            }
+        }
+
+        public char[,] GetBoard()
+        {
+            return _board;
+        }
+    }
+}
--- TicTacToeTest/UnitTest1.cs
+++ TicTacToeTest/UnitTest1.cs
@@ -1,11 +1,25 @@
+using TicTacToe;
+
 namespace TicTacToeTest
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void GameBoard_ShouldBeInitializedCorrectly()
         {
+            // Arrange
+            var game = new TicTacToeGame();
 
+            // Act
+            var board = game.GetBoard();
+
+            // Assert
+            Assert.Equal(3, board.GetLength(0));
+            Assert.Equal(3, board.GetLength(1));
+            foreach (var cell in board)
+            {
+                Assert.Equal(' ', cell);
+            }
         }
     }
 }
